821f05f873ef7b9932d22ec6d84e78f0faf56638
charith0901 <charithjayasankha@gmail.com>
2024-07-15 00:32:30 +0530

    Migrations for TimeTable
    done migrations and model for timetable

---
+++ ruhuna-schedule-ease/app/Http/Controllers/TimeTableController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TimeTable;
+use App\Models\Course;
+use App\Http\Requests\StoreTimeTableRequest;
+use App\Http\Requests\UpdateTimeTableRequest;
+use Illuminate\Http\RedirectResponse;
+use Inertia\Inertia;
+use Illuminate\Http\Request;
+
+class TimeTableController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $courses = Course::all();
+         return Inertia::render('TimeTable/Create', ['courses' => $courses]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTimeTableRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TimeTable $timeTable)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TimeTable $timeTable)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTimeTableRequest $request, TimeTable $timeTable)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TimeTable $timeTable)
+    {
+        //
+    }
+}
