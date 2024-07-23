763bdf9bd62287b1c7add28ee9db0026cef449e3
charith0901 <charithjayasankha@gmail.com>
2024-07-23 23:45:21 +0530

    Update web.php

--- ruhuna-schedule-ease/routes/web.php
+++ ruhuna-schedule-ease/routes/web.php
@@ -6,9 +6,6 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
-<<<<<<< HEAD
-use App\Http\Controllers\TimetableController;
-=======
 use App\Http\Controllers\DegreeProgramController;
 use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\LectureHallController;
@@ -17,7 +14,6 @@ use App\Http\Controllers\TimetableController;
 use App\Http\Controllers\SemesterController;
 
 
->>>>>>> main
 
 
 Route::middleware(['auth'])->group(function () {
@@ -30,15 +26,10 @@ Route::middleware(['auth'])->group(function () {
     Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
     Route::post('/import-users', [UserController::class, 'import'])->name('users.import');
     Route::post('/users/storeMany', [UserController::class, 'storeMany'])->name('users.storeMany');
-<<<<<<< HEAD
-
-
-=======
     Route::resource('degree-programs', DegreeProgramController::class);
     Route::get('/users/export', [UserController::class, 'export'])->name('users.export');
     Route::resource('lecture-halls', LectureHallController::class);
     Route::resource('semesters', SemesterController::class);
->>>>>>> main
 
 });
 
@@ -64,12 +55,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::resource('roles', RoleController::class);
-<<<<<<< HEAD
-    Route::get('/timetable',[TimetableController::class,'create'])->name('timetable.create');
-=======
     Route::resource('permissions', PermissionController::class);
-   
->>>>>>> main
+
 });
 
 
