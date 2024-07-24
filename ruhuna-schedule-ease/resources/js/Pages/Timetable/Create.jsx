26e6a8e6b452859e95bc560203a04d00008f5a9e
charith0901 <charithjayasankha@gmail.com>
2024-07-24 20:34:42 +0530

    Update Create.jsx

--- ruhuna-schedule-ease/resources/js/Pages/Timetable/Create.jsx
+++ ruhuna-schedule-ease/resources/js/Pages/Timetable/Create.jsx
@@ -1,22 +1,37 @@
-import React from 'react'
 import Authenticated from '@/Layouts/AuthenticatedLayout'
-import { Head } from "@inertiajs/react";
-import SearchInput from '@/Components/SearchInput';
-import PrimaryButton from '@/Components/PrimaryButton';
+import React from 'react'
+import Select from 'react-select';
+import { useState,useEffect } from 'react';
+import { useForm,Head,usePage } from '@inertiajs/react'
 
-export default function Create({auth}) {
+export default function Create({auth,courses,lecturers}) {
+
+  const courseOptions = courses.map((course) => ({ value: course, label: course.name }));
+  const lecturerOptions = lecturers.map((lecturer) => ({ value: lecturer, label: lecturer.name }));
   return (
-        <Authenticated
-            user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">TimeTable</h2>}
-        >
+    <Authenticated
+        user={auth.user}
+        header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Create</h2>}
+    >
         <Head title="TimeTable" />
-                <div>
-                    <h1>H!</h1>
-                    <SearchInput />
-                   
-                </div>
-        </Authenticated>
+        <div>
+          <h1>TimeTable Create</h1>
+          <form onSubmit={handleSubmit}>
+          <h1>Select a Course</h1>
+              <Select
+                options={courseOptions}
+                placeholder="Select a course..."
+                isClearable
+              />
+              <Select
+                options={lecturerOptions}
+              placeholder="Select a lecturer..."
+              isClearable
+              />
+            <button type="submit">Create TimeTable</button>
+          </form>
 
+        </div>
+    </Authenticated>
   )
 }
