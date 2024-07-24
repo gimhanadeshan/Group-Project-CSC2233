ad5ad95893b506a93c521364260d7073e8236ce8
charith0901 <charithjayasankha@gmail.com>
2024-07-24 20:34:32 +0530

    Create 2024_07_24_143017_create_time_tables_table.php

---
+++ ruhuna-schedule-ease/database/migrations/2024_07_24_143017_create_time_tables_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('time_tables', function (Blueprint $table) {
+            $table->id('slot_id');
+            $table->timestamp('start_time')->nullable();
+            $table->timestamp('end_time')->nullable();
+            $table->enum('day_of_week', ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']);
+            $table->foreignId('course')->constrained('courses')->onDelete('cascade');
+            $table->foreignId('hall_id')->condtrained('lecture_halls')->onDelete('cascade');
+            $table->foreignId('lecturer')->constrained('users')->onDelete('cascade');
+            $table->foreignId('semester_id')->constrained('semesters')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('time_tables');
+    }
+};
