d8a74b1f75ff1700cc1a0944ac8cd41e67d4c4e1
charith0901 <charithjayasankha@gmail.com>
2024-07-24 22:18:56 +0530

    Update DatabaseSeeder.php

--- ruhuna-schedule-ease/database/seeders/DatabaseSeeder.php
+++ ruhuna-schedule-ease/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Course;
 use App\Models\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,26 +15,26 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-   
+
     public function run(): void
     {
         // User::factory(10)->create();
 
-       
+
 
         $this->call([RoleSeeder::class]);
-
+        $this->call([CourseSeeder::class]);
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
             'registration_no'=>'sc11111',
             'role_id'=>1,
-           
+
         ]);
 
-       
-       
+
+
     }
 
-    
+
 }
