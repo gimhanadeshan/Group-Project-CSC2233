15be4158991478351d5b5425144066dd1dedcbeb
gimhanadeshan <gimhanadeshan00@gmail.com>
2024-07-28 16:14:42 +0530

    Update SemesterController.php

--- ruhuna-schedule-ease/app/Http/Controllers/SemesterController.php
+++ ruhuna-schedule-ease/app/Http/Controllers/SemesterController.php
@@ -99,6 +99,14 @@ class SemesterController extends Controller
         }
     }
 
+    public function show(Semester $semester)
+    {
+        return Inertia::render('Semesters/Show', [
+            'semester' => $semester,
+            'auth' => auth()->user(),
+        ]);
+    }
+
     public function destroy(Semester $semester)
     {
         try {
